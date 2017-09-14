d5e35683f77ba4dbcfea919a5b36cb01ced9035d
Valentyn Khalin <vkhalin@mirantis.com>
2017-09-14 14:30:43 +0300

    project skeleton beginning

---
+++ src/main/java/pages/Page.java
@@ -0,0 +1,19 @@
+import org.apache.commons.codec.binary.Base64;
+import org.openqa.selenium.WebDriver;;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class Page {
+
+    public static WebDriver driver = DriverBuilder.INSTANCE.getDriver();
+
+    protected Page() {
+        Config.loadProperties("Page.properties");
+        PageFactory.initElements(driver, this);
+    }
+
+    private String decrypt(String encryptedString) {
+        byte[] byteArray = Base64.decodeBase64(encryptedString.getBytes());
+        return new String(byteArray);
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+
+public class TestBase extends Page {
+
+    public HomePage homePage = new HomePage();
+    public SearchResultPage searchResultPage = new SearchResultPage();
+    public LoginPage loginPage = new LoginPage();
+    public Page page = new Page();
+
+    @BeforeTest
+    @BeforeMethod
+    public void openHomePage() {
+        driver.get(Config.getSetting("homepageURL"));
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.close();
+    }
+}
